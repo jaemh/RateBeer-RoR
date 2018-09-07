40b3a69f64efbf6fbfc6bffe86e974b701056ace
jaem <jaana.embrich-hakala@helsinki.fi>
2018-09-07 19:57:57 +0300

    muokattu modelia, tehtävä 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_142809) do
+ActiveRecord::Schema.define(version: 2018_09_07_160720) do
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "breweries", force: :cascade do |t|
     t.string "name"
