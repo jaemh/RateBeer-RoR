b9dc72ca5a8b6c8887eb00c63d47b230face9e76
jaem <jaana.embrich-hakala@helsinki.fi>
2018-09-30 22:58:53 +0300

    .

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-require 'helpers'
 require 'simplecov'
 SimpleCov.start('rails')
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -8,6 +7,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
